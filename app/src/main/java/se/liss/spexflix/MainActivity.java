35fd8c40e095b285230e62dd70b89d33c9754ecf
Daniel Häggmyr <daniel.haggmyr@attentec.se>
2020-04-21 19:27:26 +0200

    Content is now in fragment instead

--- app/src/main/java/se/liss/spexflix/MainActivity.java
+++ app/src/main/java/se/liss/spexflix/MainActivity.java
@@ -2,6 +2,10 @@ package se.liss.spexflix;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,47 +24,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import se.liss.spexflix.data.ShowData;
+import se.liss.spexflix.showPicker.ShowPickerFragment;
 import se.liss.spexflix.videoCard.VideoCardAdapter;
 import se.liss.spexflix.videoCard.VideoCardDecorator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.main_recycler_view);
-        VideoCardAdapter adapter = new VideoCardAdapter(this);
-        recyclerView.addItemDecoration(new VideoCardDecorator(this));
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        Gson gson = new Gson();
-        String jsonString = null;
-        AssetManager assetManager = getAssets();
-        try {
-            for (String asset : assetManager.list("")) {
-                if (asset.endsWith(".json")) {
-                    InputStream is = getAssets().open(asset);
-
-                    int size = is.available();
-                    byte[] buffer = new byte[size];
-                    is.read(buffer);
-                    is.close();
-
-                    jsonString = new String(buffer, "UTF-8");
-                    break;
-                }
-            }
-
-            if (jsonString != null) {
-                Type showDataType = new TypeToken<List<ShowData>>() {}.getType();
-                List<ShowData> dataList = gson.fromJson(jsonString, showDataType);
-                adapter.setData(dataList);
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Load error", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        ShowPickerFragment newFragment = new ShowPickerFragment();
+        transaction.add(R.id.main_fragment, newFragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/se/liss/spexflix/showPicker/ShowPickerFragment.java
@@ -0,0 +1,84 @@
+package se.liss.spexflix.showPicker;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import se.liss.spexflix.R;
+import se.liss.spexflix.data.ShowData;
+import se.liss.spexflix.videoCard.VideoCardAdapter;
+import se.liss.spexflix.videoCard.VideoCardDecorator;
+
+public class ShowPickerFragment extends Fragment {
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.show_picker_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        context = getContext();
+
+        RecyclerView recyclerView = getView().findViewById(R.id.main_recycler_view);
+        VideoCardAdapter adapter = new VideoCardAdapter(context);
+        recyclerView.addItemDecoration(new VideoCardDecorator(context));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        Gson gson = new Gson();
+        String jsonString = null;
+        AssetManager assetManager = context.getAssets();
+        try {
+            for (String asset : assetManager.list("")) {
+                if (asset.endsWith(".json")) {
+                    InputStream is = getContext().getAssets().open(asset);
+
+                    int size = is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+
+                    jsonString = new String(buffer, "UTF-8");
+                    break;
+                }
+            }
+
+            if (jsonString != null) {
+                Type showDataType = new TypeToken<List<ShowData>>() {}.getType();
+                List<ShowData> dataList = gson.fromJson(jsonString, showDataType);
+                adapter.setData(dataList);
+            }
+        } catch (Exception e) {
+            Toast.makeText(context, "Load error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
